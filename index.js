69052fec1d25df2c89ad6d79bba06001b5c96351
Jonathan Belloit <Jonathan@topdogwebdev.com>
2024-10-10 16:21:50 -0400

    ready to add commands

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const { Client, GatewayIntentBits, Events } = require('discord.js');
+const { Client, GatewayIntentBits, Events, ActivityType } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] });
 const { token } = require('./config.json');
 
 client.once(Events.ClientReady, clientReady => {
+  clientReady.user.setActivity(`Studying Scripture!`, { type: ActivityType.Custom });
   console.log(`Ready! Logged in as ${clientReady.user.tag}`);
 });
 
-client.login(token);
+client.on(Events.MessageCreate, message => {
+  if (message.author.bot) return;
 
-// testing
+  if (message.content === 'ping') {
+    message.channel.send('pong');
+  }
+});
+
+client.login(token);
