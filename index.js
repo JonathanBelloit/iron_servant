96636437ef5d596d7b8b9650875cd2ae253451f7
Jonathan Belloit <Jonathan@topdogwebdev.com>
2024-10-13 21:20:18 -0400

    Created new structure, refactor in progress

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client, GatewayIntentBits, Events, ActivityType } = require('discord.js');
+const eventHandler = require('./src/handlers/eventHandler');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] });
 // const { token } = require('./config.json');
@@ -7,17 +8,36 @@ client.config = require('./config.json');
 // client.cache = new Map();
 
 // require('./utils/RegisterCommands')(client);
+require('./src/utils/RegisterCommands');
 
-client.once(Events.ClientReady, clientReady => {
-  clientReady.user.setActivity(`Studying Scripture!`, { type: ActivityType.Custom });
-  console.log(`Ready! Logged in as ${clientReady.user.tag}`);
-});
+eventHandler(client);
 
-client.on(Events.MessageCreate, message => {
-  if (message.author.bot) return;
+// client.once(Events.ClientReady, clientReady => {
+//   clientReady.user.setActivity(`Studying Scripture!`, { type: ActivityType.Custom });
+//   console.log(`Ready! Logged in as ${clientReady.user.tag}`);
+// });
 
-  if (message.content === 'ping') {
-    message.channel.send('pong');
+// client.on(Events.MessageCreate, message => {
+//   if (message.author.bot) return;
+
+//   if (message.content === 'ping') {
+//     message.channel.send('pong');
+//   }
+
+//   if (message.content === 'add') {
+//     const num1
+//   }
+// });
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+  if (interaction.commandName === 'ping') {
+    interaction.reply('Pong!');
+  };
+  if (interaction.commandName === 'add') {
+    const num1 = interaction.options.getNumber('first-number');
+    const num2 = interaction.options.getNumber('second-number');
+    interaction.reply(`${num1} + ${num2} = ${num1 + num2}`);
   }
 });
 
---
+++ src/utils/RegisterCommands.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const { REST, Routes, ApplicationCommandOptionType } = require('discord.js');
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!'
+  },
+  {
+    name: 'add',
+    description: 'Adds two numbers together.',
+    options: [
+      {
+        name: 'first-number',
+        description: 'The first number to add.',
+        type: ApplicationCommandOptionType.Number,
+        required: true
+      },
+      {
+        name: 'second-number',
+        description: 'The second number to add.',
+        type: ApplicationCommandOptionType.Number,
+        required: true
+      }
+    ]
+  }
+]
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Registering slash commands...');
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands });
+    console.log('Slash commands registered!');
+  } catch (error) {
+    
+    console.error(`Error: ${error}`);
+  }
+  })();
