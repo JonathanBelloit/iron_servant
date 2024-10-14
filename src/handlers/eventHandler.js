964cdd29d25662245c375dedc876823daa5ebde3
Jonathan Belloit <Jonathan@topdogwebdev.com>
2024-10-14 04:46:06 -0400

    working out eventHandler.js, and added getAllFiles function to loop through files and pull out functions

--- src/handlers/eventHandler.js
+++ src/handlers/eventHandler.js
@@ -1,1 +1,19 @@
-module.exports = (client) => { };
+const path = require('path');
+const getAllFiles = require("../utils/getAllFiles");
+
+module.exports = (client) => { 
+  const eventFolders = getAllFiles(path.join(__dirname, '..', 'events'), true)
+
+  for (const eventFolder of eventFolders) {
+    const eventFiles = getAllFiles(eventFolder);
+    
+    const eventName = eventFolder.split('/').pop();
+    
+    client.on(eventName, async (arg) => {
+      for (const eventFile of eventFiles) {
+        const eventFunction = require(eventFile);
+        await eventFunction(client, arg);
+      }
+    })
+  }
+};
